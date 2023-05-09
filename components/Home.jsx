81a3934947a08c41f021e80f7f70bae7e36ff48a
cong_wa <cong_wa@163.com>
2023-05-09 19:22:22 +0800

    feat: index

--- components/Home.jsx
+++ components/Home.jsx
@@ -2,11 +2,17 @@
 import React from "react";
 import hero from "./assets/hero.png";
 import { FaArrowRight } from "react-icons/fa";
-import { heroIcons } from "./Data";
 import Image from 'next/image'
+import { productListReq } from '@/services/index';
 
 
-const Home = () => {
+const Home = async () => {
+
+  const ProjectList = await productListReq()
+
+
+  const project = ProjectList?.list[0]
+  console.log(JSON.stringify(project, null, 2))
   return (
     <div
       className="section flex items-center xl:justify-center justify-start flex-wrap"
@@ -14,13 +20,13 @@ const Home = () => {
     >
       <div className="flex flex-col items-start gap-10">
         <div className="sm:text-[2.5rem] text-[1.8rem] font-bold">
-          Delicious <br /> Food is Waiting <br /> For you
+          博迈德生物 <br /> 欢迎您 <br /> 
         </div>
         <div className="btn">
-          <a href="#">View Menu</a>
+          <a href="#">查看菜单</a>
           <FaArrowRight />
         </div>
-        <div className="flex md:gap-6 gap-2">
+        {/* <div className="flex md:gap-6 gap-2">
           {heroIcons.map((heroIcon, index) => {
             return (
               <div
@@ -31,10 +37,10 @@ const Home = () => {
               </div>
             );
           })}
-        </div>
+        </div> */}
       </div>
-      <div className="min-w-[200px] justify-self-center md:w-[600px]">
-        <Image object-fit="true" src={hero} alt="3334" />
+      <div className="min-w-[200px] justify-self-center md:w-[600px] mt-6">
+        <Image object-fit="true" width={200} height={200} src={project.image} alt={project.name} />
       </div>
     </div>
   );
